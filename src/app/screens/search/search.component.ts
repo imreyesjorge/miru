beb70a0e342ed018b3b225f1c1da4cae7f51c15c
Jorge Reyes <reyestrevjorge@gmail.com>
2021-12-20 21:32:55 -0600

    Connect Search page to API

--- src/app/screens/search/search.component.ts
+++ src/app/screens/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { switchMap, tap } from 'rxjs/operators';
+import { BaseService } from 'src/app/services/base.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -10,16 +11,23 @@ export class SearchComponent implements OnInit {
   public paramQuery: string;
   public searchResults: any[];
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private baseService: BaseService) {
     this.paramQuery = '';
     this.searchResults = [];
   }
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe({
-      next: (response) => {
-        this.paramQuery = response.term;
-      },
-    });
+    this.route.queryParams
+      .pipe(
+        switchMap((result) => {
+          this.paramQuery = result.term;
+          return this.baseService.searchAnime(result.term);
+        })
+      )
+      .subscribe({
+        next: (response: any) => {
+          this.searchResults = response.results;
+        },
+      });
   }
 }
