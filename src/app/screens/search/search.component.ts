d5be18703f06bf037b6642d9f7e6974448c59f6d
Jorge Reyes <reyestrevjorge@gmail.com>
2021-12-20 21:07:06 -0600

    Handle query params

--- src/app/screens/search/search.component.ts
+++ src/app/screens/search/search.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+  public paramQuery: string;
 
-  constructor(private route: ActivatedRoute) { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
+    this.paramQuery = '';
   }
 
+  ngOnInit(): void {
+    this.route.queryParams.subscribe({
+      next: (response) => {
+        this.paramQuery = response.term;
+      },
+    });
+  }
 }
